ac81fcf07df159fd427a9c496d53b5c921f68910
ab0mbs <2937017+ab0mbs@users.noreply.github.com>
2025-01-18 13:14:37 -0500

    qmk_userspace

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/ab0mbs/rules.mk
@@ -0,0 +1,13 @@
+VIA_ENABLE = yes
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+
+#TD_LSFT_CAPSLOCK_ENABLE = no
+IDLE_TIMEOUT_ENABLE = yes
+STARTUP_NUMLOCK_ON = no
+ENCODER_DEFAULTACTIONS_ENABLE = no
+
+COLEMAK_LAYER_ENABLE = no  #Enable Colemak layer / set to no to disable
+INVERT_NUMLOCK_INDICATOR = no
+EXTRAKEY_ENABLE = yes # Enable extra keys
